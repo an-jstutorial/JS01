ee07cbe2e02bf53e61077ac15ce413a48ec02731
Ashutosh <ashekha1@adient.com>
2020-05-06 13:47:00 -0400

    new student

---
+++ vikalp/scripts/sFirst.js
@@ -0,0 +1,13 @@
+
+//Write your code below....
+drawText('This is my workspace');
+line(100, 100, 200, 200);
+drawText('This is my workspace');
+drawText('hi my name is calliou  nice to meet you.' , 300, 150, 30);
+circle(300, 300, 70);
+ellipse(300, 300, 40, 60);
+rect(200, 200, 200, 300);
+//drawImage('./images/kid-cartoon.jpg', 20, 20, 100);
+//drawImage('./images/rainbow.jpg', 80, 80, 100);
+drawImage('./images/caillou.png', 120, 120, 200);
+animate();
