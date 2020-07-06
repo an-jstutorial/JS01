375dc81524aeb780fdb9fefa0dbff756d705a7f7
Ashutosh Shekhar <ashutosh@fourcolorstech.com>
2020-07-06 19:17:30 -0400

    minor changes

--- firststudent/scripts/sFirst.js
+++ firststudent/scripts/sFirst.js
@@ -3,5 +3,5 @@
 line(100, 100, 200, 200);
 drawText('Hello! Javascript class' , 300, 150, 30);
 circle(300, 300, 70);
-ellipse(300, 300, 40, 60);
+ellipse(400, 300, 40, 60);
 rect(200, 200, 200, 300);
